99195b0e02393fdb0149748edfd8bf6ecdbdb863
Xinyue <wangxinyue0198@outlook.com>
2025-10-14 21:05:04 -0700

    Finish login and main page

--- src/components/site-navbar.js
+++ src/components/site-navbar.js
@@ -1,17 +1,23 @@
-class SiteNavbar extends HTMLElement {
-    constructor() {
-        super();
-        this.renderNavbar();
-    }
+// Import specific functions from the Firebase Auth SDK
+import { onAuthStateChanged } from "firebase/auth";
 
-    renderNavbar() {
-        this.innerHTML = `
+import { auth } from "/src/firebaseConfig.js";
+import { logoutUser } from "/src/authentication.js";
+class SiteNavbar extends HTMLElement {
+  constructor() {
+    super();
+    this.renderNavbar();
+    this.renderAuthControls();
+  }
+
+  renderNavbar() {
+    this.innerHTML = `
             <!-- Navbar: single source of truth -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="/">
-                        <img src="/images/image.jpg" height="36">
-                        ElmoHikes
+                        <img src="../../public/images/logo.png" height="36">
+                        XinyueHikes
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,7 +43,26 @@ class SiteNavbar extends HTMLElement {
                 </div>
             </nav>
         `;
-    }
+  }
+  renderAuthControls() {
+    const authControls = this.querySelector("#authControls");
+
+    // Initialize with invisible placeholder to maintain layout space
+    authControls.innerHTML = `<div class="btn btn-outline-light" style="visibility: hidden; min-width: 80px;">Log out</div>`;
+
+    onAuthStateChanged(auth, (user) => {
+      let updatedAuthControl;
+      if (user) {
+        updatedAuthControl = `<button class="btn btn-outline-light" id="signOutBtn" type="button" style="min-width: 80px;">Log out</button>`;
+        authControls.innerHTML = updatedAuthControl;
+        const signOutBtn = authControls.querySelector("#signOutBtn");
+        signOutBtn?.addEventListener("click", logoutUser);
+      } else {
+        updatedAuthControl = `<a class="btn btn-outline-light" id="loginBtn" href="/login.html" style="min-width: 80px;">Log in</a>`;
+        authControls.innerHTML = updatedAuthControl;
+      }
+    });
+  }
 }
 
-customElements.define('site-navbar', SiteNavbar);
+customElements.define("site-navbar", SiteNavbar);
